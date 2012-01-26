39dbf46637236449595633c118f696dee3c37121
Liz Woods <lizwoods.ands@gmail.com>
2012-01-27 10:56:45 +1100

    SD-1468 made changes so that the % character could be used in the description fields

--- registry/src/orca/manage/_processes/save.php
+++ registry/src/orca/manage/_processes/save.php
@@ -30,9 +30,8 @@ if($json)
 	$dateCreated = urldecode($test_array['mandatoryInformation']['dateAccessioned']);
 	$xml__text = $a2xml->array2xml($test_array);
 	$xml__text = str_replace ('%26', '&amp;', $xml__text);
-	$xml__text = str_replace ('%', '\\', $xml__text);	
-	$xml__text = preg_replace_callback('/\\\\u([0-9a-f]{4})/i','replace_unicode_escape_sequence', $xml__text );					
-	$xml__text = preg_replace_callback('/\\\\([A-F0-9]{2})/i','replace_unicode_escape_sequence2', $xml__text );
+	$xml__text = preg_replace_callback('/\\%u([0-9a-f]{4})/i','replace_unicode_escape_sequence', $xml__text );					
+	$xml__text = preg_replace_callback('/\\%([A-F0-9]{2})/i','replace_unicode_escape_sequence2', $xml__text );
 
 	$rifcs->loadXML(trim($xml__text));
 
