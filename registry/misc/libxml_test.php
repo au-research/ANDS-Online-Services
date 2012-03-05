cc5e3c871a5927098b9885eea6c4ddcb23c39ac7
bgreenwood <ben.greenwood@anu.edu.au>
2012-03-05 12:34:21 +1100

    Add a test suite for libxml compatibility

---
+++ registry/misc/libxml_test.php
@@ -0,0 +1,12 @@
+<?php
+
+ $registryObjects = new DOMDocument();
+ $registryObjects->loadXML('<registryObjects><registryObject group="Hello World"></registryObject></registryObjects>');
+ $registryObjects->schemaValidate('http://services.ands.org.au/documentation/rifcs/1.3/schema/registryObjects.xsd');
+ 
+$errors = error_get_last();
+if( $errors )
+{
+	echo "Document Validation Error: ".$errors['message']."\n";
+}
+ 
