4a8e3b8add612eedab4293af1ae8994740e2c314
lmonus <leo.monus@anu.edu.au>
2012-03-20 16:12:58 +1100

    QA table for meta-metadata

--- registry/db/caching_layer/dbs_orca_caching_layer.sql
+++ registry/db/caching_layer/dbs_orca_caching_layer.sql
@@ -4,16 +4,22 @@ CREATE TABLE dba.tbl_registry_objects_qa_results
   draft_key character varying(512),
   registry_object_data_source character varying(255),
   registry_object_key character varying(512),
-  result_type character varying(64),
-  result_value text,
+  flag smallint NOT NULL DEFAULT 0,
+  gold_status smallint NOT NULL DEFAULT 0,
+  quality_test_result text,
+  warning_count integer,
+  error_count integer,
+  quality_level integer,
+  quality_level_result text,
+  status character varying,
   date_checked bigint DEFAULT date_part('epoch'::text, now()),
   CONSTRAINT pk_tbl_registry_objects_qa_results PRIMARY KEY (qa_id ),
   CONSTRAINT fk_tbl_registry_objects_qa_results_1 FOREIGN KEY (draft_key, registry_object_data_source)
       REFERENCES dba.tbl_draft_registry_objects (draft_key, registry_object_data_source) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
+      ON UPDATE NO ACTION ON DELETE CASCADE,
   CONSTRAINT fk_tbl_registry_objects_qa_results_2 FOREIGN KEY (registry_object_key)
       REFERENCES dba.tbl_registry_objects (registry_object_key) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
+      ON UPDATE NO ACTION ON DELETE CASCADE
 )
 WITH (
   OIDS=FALSE
@@ -26,38 +32,13 @@ COMMENT ON TABLE dba.tbl_registry_objects_qa_results
   IS 'meta-meta data about Registry Objects';
 
 
---delete from dba.tbl_registry_objects_qa_results;
+INSERT INTO dba.tbl_registry_objects_qa_results
+(draft_key, registry_object_data_source, error_count, warning_count, status, quality_test_result,flag)
+SELECT draft_key, registry_object_data_source, error_count, warning_count, status, quality_test_result, (CASE flag WHEN 't' then 1 ELSE 0 END)
+from dba.tbl_draft_registry_objects;
+
 
 INSERT INTO dba.tbl_registry_objects_qa_results
-(draft_key, registry_object_data_source, result_type, result_value)
-SELECT draft_key, registry_object_data_source,'error_count',error_count
-from dba.tbl_draft_registry_objects d where d.error_count is not null;
-
-INSERT INTO dba.tbl_registry_objects_qa_results
-(draft_key, registry_object_data_source, result_type, result_value)
-SELECT draft_key, registry_object_data_source,'warning_count',warning_count
-from dba.tbl_draft_registry_objects d where d.warning_count is not null;
-
-INSERT INTO dba.tbl_registry_objects_qa_results
-(draft_key, registry_object_data_source, result_type, result_value)
-SELECT draft_key, registry_object_data_source,'quality_test_result',quality_test_result
-from dba.tbl_draft_registry_objects d where d.quality_test_result is not null;
-
-INSERT INTO dba.tbl_registry_objects_qa_results
-(registry_object_key, registry_object_data_source, result_type, result_value)
-SELECT registry_object_key, data_source_key,'error_count',error_count
-from dba.tbl_registry_objects d where d.error_count is not null;
-
-INSERT INTO dba.tbl_registry_objects_qa_results
-(registry_object_key, registry_object_data_source, result_type, result_value)
-SELECT registry_object_key, data_source_key,'warning_count',warning_count
-from dba.tbl_registry_objects d where d.warning_count is not null;
-
-INSERT INTO dba.tbl_registry_objects_qa_results
-(registry_object_key, registry_object_data_source, result_type, result_value)
-SELECT registry_object_key, data_source_key,'quality_test_result',quality_test_result
-from dba.tbl_registry_objects d where d.quality_test_result is not null;
-
-
---select * from dba.tbl_registry_objects_qa_results where draft_key = 'DCB0066';
---select * from dba.tbl_registry_objects_qa_results where registry_object_key = '03c59e36-bee4-4186-8bd4-f1c5dd69c396';
+(registry_object_key, registry_object_data_source, error_count, warning_count, status, quality_test_result,flag)
+SELECT registry_object_key, data_source_key, error_count, warning_count, status, quality_test_result, (CASE flag WHEN 't' then 1 ELSE 0 END)
+from dba.tbl_registry_objects d;
