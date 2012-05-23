e005369bd37f3209393d958c0c750bf99c47e85f
Ben Greenwood <ben.greenwood@anu.edu.au>
2012-05-23 17:40:41 +1000

    First crack at topic page prototype

---
+++ registry/src/orca/rda/application/models/topics.php
@@ -0,0 +1,40 @@
+<?php
+class Topics extends CI_Model {
+
+	private $topics = array (
+
+			"tropics" =>
+			array(
+					"name"=>"Tropical Sciences",
+					"html"=>"blah blah",
+					"boxes" => array(
+							array ("id"=>"collections",
+									"query"=>"*:*",
+									"heading"=>"Collections",
+									"record_limit"=>3),
+							array ("id"=>"groups",
+									"query"=>'type:("group")',
+									"heading"=>"Research Groups",
+									"record_limit"=>3),
+							array ("id"=>"services",
+									"query"=>'class:("service")',
+									"heading"=>"Related Services",
+									"record_limit"=>3),
+
+					),
+			),
+
+
+	);
+
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	function getTopics()
+	{
+		return $this->topics;
+	}
+}
