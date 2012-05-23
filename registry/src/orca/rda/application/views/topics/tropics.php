91bf157dab92bdd46fae9041f94db4a03cf1d206
Ben Greenwood <ben.greenwood@anu.edu.au>
2012-05-23 17:40:41 +1000

    First crack at topic page prototype

---
+++ registry/src/orca/rda/application/views/topics/tropics.php
@@ -0,0 +1,15 @@
+<i>This topic page provides an overview of a selection of relevant data collections within RDA covering tropical data. Use the links on the left to access the collections, services and groups related to tropical data. You can also browse by subject from this page.</i>
+
+<h3>Australian Research and Key Research Areas</h3>
+    
+Australia is unique amongst developed countries in having a significant landmass in the tropics. Tropical Australia accounts for 46% of the nation's landmass and despite its low and dispersed population base, is the source of more than 30% of the nation's exports, particularly from mining, oil and gas. Six per cent of Australians live in the tropics, including close to one-third of indigenous Australians.
+<br/><br/>
+In an era of climate change, tropical issues are of growing global significance. Approximately one-half of the world's population lives in the tropics. With population growth double the world average, the tropics are under severe and growing pressure. 
+<br/><br/>
+A high proportion of the world's biodiversity is located in the tropics, including up to 80% of animal and plant species and 92% of world's coral reefs. Population growth and urbanisation have led to biodiversity loss on a massive scale in many tropical countries, a process exacerbated by climate change. 
+<br/><br/>
+High population density and growth are similarly linked to poor health outcomes and to the emergence of new infectious diseases. Seventy-five per cent of new infectious diseases are zoonoses, diseases of animals that are transmitted to humans (e.g. SARS and avian influenza). It is a process exacerbated by humans living in close physical proximity to domesticated animals and by global warming which changes the distribution of disease insect vectors (e.g., mosquitoes). 
+<br/><br/>
+To meet the needs of researchers in this area, James Cook University has developed a Tropical Data Hub as an 'open' portal, with contributors submitting data sets and other content in an open and collaborative way. The portal is intended to complement existing data repositories and stores and be a banner under which a variety of state, commonwealth and university research programs with interests in the tropics can be concentrated. In addition the hub provides a data hosting capacity in order to host significant national and international data sets. As well as hosting and exposing data, a key functionality of the portal is data searching (including via GIS methods) across disparate data sets.
+<br/><br/>
+<span style="font-size:0.8em;font-decoration:italics;">[Source: James Cook University, http://www.jcu.edu.au/research/JCUPRD_025773.html]</span>
