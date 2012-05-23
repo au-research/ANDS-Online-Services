b08452a6b9d93ea8e19c1ba14467eb50c56adee0
Ben Greenwood <ben.greenwood@anu.edu.au>
2012-05-23 18:01:03 +1000

    Topic list and global template modifications

---
+++ registry/src/orca/rda/application/views/topic-list.php
@@ -0,0 +1,28 @@
+<div id="top" class="top-corner">
+
+	<ul id="breadcrumb" class="top-corner">
+		<li><a href="<?php echo base_url(); ?>" class="crumb">Home</a></li>
+		<li><a href="#" class="crumb">Topics</a></li>
+	</ul>
+
+</div>
+
+<div id="item-view-inner" class="clearfix">
+
+<div id="left">
+ 	<div id="displaytitle"><h1 itemprop="name">Topics listed in Research Data Australia</h1></div>
+ 	<div class="clearfix"></div>
+
+ 	<ul>
+ 	<?php
+ 	foreach ($topics AS $key => $data)
+ 	{
+ 		echo "<li><a href='" . base_url() . "topic/" . $key ."'>".$data['name']."</a></li>";
+ 	}
+ 	?>
+ 	</ul>
+</div>
+
+</div>
+
+</div>
