5bb0579c95bdc82b28dbf787653d339244b997e1
Elizabeth Woods <u3485947@ands-dev.anu.edu.au>
2012-06-01 12:24:29 +1000

    Testing code removed

--- registry/src/orca/rda/application/views/search/seeAlsoDataCite.php
+++ registry/src/orca/rda/application/views/search/seeAlsoDataCite.php
@@ -19,7 +19,7 @@ limitations under the License.
 <p>
 <?php
 //echo "<hr style='margin:10px;'/>";
-echo "<h3 style='margin:10px;border-top:1px dashed #333333;padding-top:10px;'> <img src='".base_url()."img/icon/link_64.png' style='width:16px;'> External Websites:</h3>";
+echo "<h3 style='margin:10px;border-top:1px dashed #333333;padding-top:10px;'> <img src='".base_url()."img/icon/link_64.jpg' style='width:16px;'> External Websites:</h3>";
 echo '<p><a href="javascript:void(0);" id="seeAlso_DataCiteNumFound"><span id="seealsodatacite-realnumfound">'.$numfound.'</span> Collections </a> from <span id="seeAlso_dataciteInfo">DataCite</span></p>';
 
 //echo 'Also related from Subjects: <a href="javascript:void(0);" id="seeAlso_subjectNumFound">'. $activityNumFound.' Collections</a>';
--- registry/src/orca/rda/js/script.js
+++ registry/src/orca/rda/js/script.js
@@ -871,10 +871,10 @@ $(document).ready(function(){
                                $("#seeAlsoDataCite").html(msg);
                                 //console.log(msg);
                                if(parseInt($('#seealso-realnumfound').html())==0&&(parseInt($('#seealsodatacite-realnumfound').html()))==0){
-	                            	//$('#seeAlsoDataCiteRightBox').hide();
+	                            	$('#seeAlsoDataCiteRightBox').hide();
 	                            }
                                if(parseInt($('#seealsodatacite-realnumfound').html())==0){
-	                            	//$('#seeAlsoDataCite').hide();
+	                            	$('#seeAlsoDataCite').hide();
 	                            }
                                
                        },
@@ -885,12 +885,12 @@ $(document).ready(function(){
 	   var seeAlsoDataCitePage = 1;
 		$('#seeAlso_dataciteInfo').live('mouseenter',function(event){
 			$("<div class='tooltip' style='background-color:#FFFFFF;border:1px #666666 solid;padding:15px' align='left'><h3>About DataCite</h3><p>Datacite is a not-for-profit orginisation formed in London on 1 December 2009.</p><p>DataCite's aim is to: </p><ul><li>Establish easier access to research data on the internet</li><li>Increase acceptance of research data as legitimate, citable contributions to the scholarly record<li>Support data archiving that will permit results to be verified and re-purposed for further study.</li></ul><p>For more information about DataCite, visit <a href='http://datacite.org' target='_blank'>http://datacite.org</a></p><hr /> <p>Disclaimer required</p></div>").appendTo('body');
-			var tPosX = event.pageX - 700;
-		    var tPosY = event.pageY - 300;
+			var tPosX = event.pageX - 650;
+		    var tPosY = event.pageY - 200;
 			 $('div.tooltip').css({'position': 'absolute', 'top': tPosY, 'left': tPosX});
 		
 		})
-		$('#seeAlso_dataciteInfo').live('mouseleave',function(){
+		$('.tooltip').live('mouseleave',function(){
 			$('.tooltip').hide();
 			
 		})		
