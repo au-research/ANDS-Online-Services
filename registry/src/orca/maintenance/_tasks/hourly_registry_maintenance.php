d6d003402430cb8dbd2ba2f828952bc1290b659d
Ben Greenwood <ben.greenwood@anu.edu.au>
2012-06-21 11:42:03 +1000

    Check and update any data source hashes

--- registry/src/orca/maintenance/_tasks/hourly_registry_maintenance.php
+++ registry/src/orca/maintenance/_tasks/hourly_registry_maintenance.php
@@ -2,6 +2,7 @@
 function task_hourly_registry_maintenance($task)
 {
 	$taskId = $task['task_id'];
+	$message = '';
 
 	// XXX: Check Harvester
 
@@ -13,9 +14,30 @@ function task_hourly_registry_maintenance($task)
 
 	// XXX: Update stats
 
-	// Clear completed tasks
+	/*
+	 * Clear completed tasks
+	 */
 	deleteCompletedTasksBefore("1 day");
 
+	/*
+	 * Check for null data sources (XXX: slugs, ro hashes)
+	 */
+	$dataSources = getDataSources(null,null);
+	foreach ($dataSources AS $ds)
+	{
+		if ($ds['key_hash'] == "")
+		{
+			// Calling this function will update the hash if it doesn't exist
+			getDataSourceHashForKey($ds['data_source_key']);
+		}
+	}
+	// Don't forget PMD!
+	getDataSourceHashForKey('PUBLISH_MY_DATA');
+
+
+	/*
+	 * Requeue for next time...
+	*/
 	addNewTask($task['method'], "Requeued task from task run: " . $taskId, '', '', null, "1 hour");
 	return $message;
 }
