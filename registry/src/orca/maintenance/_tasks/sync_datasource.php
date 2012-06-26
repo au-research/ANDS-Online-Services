60c8d629844984541be2c7503664f565b9caeb51
Ben Greenwood <ben.greenwood@anu.edu.au>
2012-06-26 16:51:21 +1000

    New sync datasource task for minh

---
+++ registry/src/orca/maintenance/_tasks/sync_datasource.php
@@ -0,0 +1,17 @@
+<?php
+function task_sync_datasource($task)
+{
+	$taskId = $task['task_id'];
+	$dataSourceKey = $task['data_source_key'];
+	$message = '';
+
+
+	if ($dataSourceKey)
+	{
+		addNewTask('RUN_QUALITY_CHECK', "Requeued task from task run: " . $taskId, '', $task['data_source_key'], null);
+		addNewTask('GENERATE_CACHE', "Requeued task from task run: " . $taskId, '', $task['data_source_key'], null);
+		addNewTask('INDEX_RECORDS', "Requeued task from task run: " . $taskId, '', $task['data_source_key'], null);
+	}
+
+    return $message;
+}
