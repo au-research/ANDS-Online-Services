c12167c55c30c45c541fba169045f406a285a105
Ben Greenwood <ben.greenwood@anu.edu.au>
2012-06-27 14:32:20 +1000

    Improve responsiveness of *_all tasks (i.e cache all)

--- registry/src/orca/maintenance/_tasks/sync_datasource.php
+++ registry/src/orca/maintenance/_tasks/sync_datasource.php
@@ -5,12 +5,15 @@ function task_sync_datasource($task)
 	$dataSourceKey = $task['data_source_key'];
 	$message = '';
 
-
 	if ($dataSourceKey)
 	{
 		addNewTask('RUN_QUALITY_CHECK', "Requeued task from task run: " . $taskId, '', $task['data_source_key'], null);
 		addNewTask('GENERATE_CACHE', "Requeued task from task run: " . $taskId, '', $task['data_source_key'], null);
 		addNewTask('INDEX_RECORDS', "Requeued task from task run: " . $taskId, '', $task['data_source_key'], null);
+	}
+	else
+	{
+		$message .= "Must provide a data source to sync...";
 	}
 
     return $message;
