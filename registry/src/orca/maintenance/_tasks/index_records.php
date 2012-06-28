64fe54f7003c2652e2e7f6e3d7797447782676ba
Leop Monus <leo.monus@anu.edu.au>
2012-06-28 14:17:28 +1000

    fixes harvest reindex draft bug

--- registry/src/orca/maintenance/_tasks/index_records.php
+++ registry/src/orca/maintenance/_tasks/index_records.php
@@ -116,8 +116,13 @@ function addDraftToSolrIndex($registryObjectKey, $commit=true)
 			{
 					$registryDateModified = time(); // default to now
 			}
-			$xml .= "      <extRif:registryDateModified>".$registryDateModified."</extRif:registryDateModified>\n";
-
+			else 
+			{
+				$registryDateModified = strtotime($registryDateModified); // parse the SQL timestamp
+			}
+			// SOLR requires the date in ISO8601, restricted to zulu time (why, I don't know...)
+			$xml .= "      <extRif:registryDateModified>".gmdate('Y-m-d\TH:i:s\Z',$registryDateModified)."</extRif:registryDateModified>\n";
+			
 
 
 			// displayTitle
@@ -247,8 +252,13 @@ function addDraftSolrIndexForDatasource($dataSourceKey)
 			{
 					$registryDateModified = time(); // default to now
 			}
-			$xml .= "      <extRif:registryDateModified>".$registryDateModified."</extRif:registryDateModified>\n";
-
+			else 
+			{
+				$registryDateModified = strtotime($registryDateModified); // parse the SQL timestamp
+			}
+			// SOLR requires the date in ISO8601, restricted to zulu time (why, I don't know...)
+			$xml .= "      <extRif:registryDateModified>".gmdate('Y-m-d\TH:i:s\Z',$registryDateModified)."</extRif:registryDateModified>\n";
+			
 
 
 			// displayTitle
--- registry/src/orca/manage/_processes/get_groups.php
+++ registry/src/orca/manage/_processes/get_groups.php
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 *******************************************************************************/
-if (!IN_ORCA) die('No direct access to this file is permitted.');
+//if (!IN_ORCA) die('No direct access to this file is permitted.');
 
 // Execute the search.
 $rawResults = getDataSources(null, null);
