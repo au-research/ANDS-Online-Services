510f7d00fd2c1222245a0de336e9bfa6a210a4fb
Ben Greenwood <ben.greenwood@anu.edu.au>
2012-07-04 14:20:55 +1000

    Fix Download RIFCS button on ORCA view

--- registry/src/orca/services/getRegistryObject.php
+++ registry/src/orca/services/getRegistryObject.php
@@ -48,35 +48,23 @@ if($type=='xml'){
 	// =============================================================================
 	require '../../_includes/finish.php';
 	
-}elseif($type=='plain'){
-	if(!$dataSource){
-		// BEGIN: XML Response
-		// =============================================================================
-		$rifcs ='<registryObjects xmlns="http://ands.org.au/standards/rif-cs/registryObjects" '."\n";
-		$rifcs .='                 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '."\n";
-		$rifcs .='                 xsi:schemaLocation="http://ands.org.au/standards/rif-cs/registryObjects '.gRIF_SCHEMA_URI.'">'."\n";
-		if( $registryObject )
-		{
-			$rifcs .= getRegistryObjectXML($registryObject[0]['registry_object_key']);
-		}
-		$rifcs .= "</registryObjects>\n";
-		// TODO : this is needed untill we stop having rifcs 1.0 elements in the database!!!
-		// so delete it once the green and orange is imp[lemented + all data is migrated to rifcs 1.2 placeholders!!
-		$rifc2 = transformToRif2XML($rifcs);
-		print $rifc2;
-	}elseif($type=='download'){
+}
+else if($type=='download')
+{
+
 		// Set the Content-Type header.
+		
 		header("Cache-Control: public"); 
 		header('Pragma: public');
-		header("Content-Type: text/xml; charset=UTF-8", true);
-		header('Content-Disposition: attachment; filename='.$registryObject[0]['registry_object_key'].'-rifcs-download.xml');
-		header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-		header("Content-Transfer-Encoding: binary");
+	//	header("Content-Type: text/xml; charset=UTF-8", true);
 		header("Content-Type: application/force-download"); 
 		header("Content-Type: application/octet-stream"); 
 		header("Content-Type: application/download"); 
+		header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+		header("Content-Transfer-Encoding: binary");
 		header("Content-Description: File Transfer"); 
-		header('Expires: 0');
+		header('Content-Disposition: attachment; filename='.$registryObject[0]['url_slug'].'-rifcs-download.xml');
+		//header('Expires: 0');
 		
 		
 		$rifcs = '<?xml version="1.0" encoding="UTF-8"?>'."\n";
@@ -93,15 +81,38 @@ if($type=='xml'){
 		$rifcs .= "</registryObjects>\n";
 		// TODO : this is needed untill we stop having rifcs 1.0 elements in the database!!!
 		// so delete it once the green and orange is imp[lemented + all data is migrated to rifcs 1.2 placeholders!!
-		$rifc2 = transformToRif2XML($rifcs);
+
+		$rifcs = transformToRif2XML($rifcs);
 		
-		header('Content-Length: ' . filesize($rifc2));
-		print $rifc2;
+		header('Content-Length: ' . filesize($rifcs));
+		print $rifcs;
 		
 		// END: XML Response
 		// =============================================================================
 		require '../../_includes/finish.php';
-	}elseif($dataSource){
+}
+elseif($type=='plain')
+{
+	
+	if(!$dataSource)
+    {
+		// BEGIN: XML Response
+		// =============================================================================
+		$rifcs ='<registryObjects xmlns="http://ands.org.au/standards/rif-cs/registryObjects" '."\n";
+		$rifcs .='                 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '."\n";
+		$rifcs .='                 xsi:schemaLocation="http://ands.org.au/standards/rif-cs/registryObjects '.gRIF_SCHEMA_URI.'">'."\n";
+		if( $registryObject )
+		{
+			$rifcs .= getRegistryObjectXML($registryObject[0]['registry_object_key']);
+		}
+		$rifcs .= "</registryObjects>\n";
+		// TODO : this is needed untill we stop having rifcs 1.0 elements in the database!!!
+		// so delete it once the green and orange is imp[lemented + all data is migrated to rifcs 1.2 placeholders!!
+		$rifc2 = transformToRif2XML($rifcs);
+		print $rifc2;
+	}
+	else
+	{
 		header("Content-Type: text/xml; charset=UTF-8", true);
 		$registryObject = getDraftRegistryObject(getQueryValue('key'), getQueryValue('ds'));
 		if (getQueryValue('stripped'))
