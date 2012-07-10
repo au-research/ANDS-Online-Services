b72b487073af99539cbd58a7f7e6ff5e53614333
Minh Nguyen <dekarvn@gmail.com>
2012-07-10 11:06:09 +1000

    fix draft key not showing on MMR.

--- registry/src/orca/_functions/orca_solr_functions.php
+++ registry/src/orca/_functions/orca_solr_functions.php
@@ -108,6 +108,7 @@ function addDraftToSolrIndex($registryObjectKey, $data_source_key, $commit=true)
 			//$xml .= '      <extRif:gold_status_flag>'.esc(trim($allKeys[$i]['gold_status_flag'])).'</extRif:gold_status_flag>'."\n";
 			$xml .= '      <extRif:quality_level>'.esc(trim($allKeys[$i]['quality_level'])).'</extRif:quality_level>'."\n";
 			$xml .= '      <extRif:feedType>'.($allKeys[$i]['draft_owner'] == 'SYSTEM' ? 'harvest' : 'manual').'</extRif:feedType>'."\n";
+			$xml .= '      <extRif:lastModifiedBy>'.$allKeys[$i]['draft_owner'].'</extRif:lastModifiedBy>'."\n";
 			$xml .= "    </extRif:extendedMetadata>\n";
 			$rifcsContent = unwrapRegistryObject($allKeys[$i]['rifcs']);
 			$rifcsContent .= $xml;
