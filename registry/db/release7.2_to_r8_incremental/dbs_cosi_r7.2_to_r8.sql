5f4b021df782549f1cf3f2c1c902a8923471e312
Ben Greenwood <ben.greenwood@anu.edu.au>
2012-07-17 12:49:34 +1000

    Fix some typos in upgrade scripts

--- registry/db/release7.2_to_r8_incremental/dbs_cosi_r7.2_to_r8.sql
+++ registry/db/release7.2_to_r8_incremental/dbs_cosi_r7.2_to_r8.sql
@@ -1,2 +1,5 @@
 INSERT INTO dba.tbl_activities (activity_id, created_who, modified_who) VALUES ('aORCA_DATA_SOURCE_REPORTS','SYSTEM','SYSTEM');
 INSERT INTO dba.tbl_role_activities (role_id, activity_id, created_who, modified_who) VALUES ('ORCA_SOURCE_ADMIN','aORCA_DATA_SOURCE_REPORTS','SYSTEM','SYSTEM');
+
+INSERT INTO dba.tbl_activities (activity_id, created_who, modified_who) VALUES ('aORCA_GOLD_INDEX','SYSTEM','SYSTEM');
+INSERT INTO dba.tbl_role_activities (role_id, activity_id, created_who, modified_who) VALUES ('PUBLIC','aORCA_GOLD_INDEX','SYSTEM','SYSTEM');
