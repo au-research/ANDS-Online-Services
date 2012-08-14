fa742e690b52989584ffd1ae3d8ee30d11d7a653
Minh Nguyen <dekarvn@gmail.com>
2012-08-14 16:01:01 +1000

    quickfix to some weird error

--- registry/db/release8_to_8.1_incremental/dbs_orca_r8_to_r8.1.sql
+++ registry/db/release8_to_8.1_incremental/dbs_orca_r8_to_r8.1.sql
@@ -5,3 +5,6 @@ ALTER TABLE dba.tbl_data_sources ADD COLUMN address_line_1 character varying(128
 ALTER TABLE dba.tbl_data_sources ADD COLUMN address_line_2 character varying(128);
 ALTER TABLE dba.tbl_data_sources ADD COLUMN city character varying(32);
 ALTER TABLE dba.tbl_data_sources ADD COLUMN state character varying(32);
+
+#HARVESTER TABLE
+ALTER TABLE public.harvest ADD COLUMN advanced_harvesting_mode CHARACTER VARYING(512) DEFAULT 'STANDARD';
