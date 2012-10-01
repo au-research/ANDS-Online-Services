75bd45543688f869f7ba7ad0b77ab64ca4f42a1f
Steven McPhillips <steven.mcphillips@gmail.com>
2012-10-02 09:21:44 +1000

    added a work-in-progress generic get for registry objects. not production-ready

--- arms/src/application/modules/registry_object/models/registry_objects.php
+++ arms/src/application/modules/registry_object/models/registry_objects.php
@@ -2,24 +2,81 @@
 
 /**
  * Core Data Sources model
- * 
+ *
  * XXX:
- * 
+ *
  * @author Ben Greenwood <ben.greenwood@ands.org.au>
  * @package ands/registryobject
- * 
+ *
  */
 
 class Registry_objects extends CI_Model {
-		
+
 	public $valid_classes = array("collection","activity","party","service");
 	public $valid_status  = array("DRAFT"=>"DRAFT", "PUBLISHED"=>"PUBLISHED", "APPROVED"=>"APPROVED", "SUBMITTED_FOR_ASSESSMENT"=>"SUBMITTED_FOR_ASSESSMENT");
 	public $valid_levels  = array("level_1"=>"1", "level_2"=>"2", "level_3"=>"3", "level_4"=>"4" );
-	
-	
+
+	private $_mkro_callback = 'return new _registry_object($r["registry_object_id"]);';
+
+	/**
+	 * Get a number of registry_objects, limited by various parameters.
+	 * If no records are found, and empty array is returned.
+	 * Note that registry_objects is joined to both data_sources and registry_object_attributes:
+	 * ensure the `$where` and `$sort` clauses are keyed accordingly.
+	 *
+	 * @param array of key-value pairs to feed into CI get_where (optional)
+	 * @param list of registry_object_ids to satisfy a 'where id in' clause (optional)
+	 * @param how many records to return (optional)
+	 * @param query offset (optional)
+	 * @param sort field (optional)
+	 * @param sort direction (optional)
+	 * @return array(_registry_object)
+	 */
+	function get($where=array(),
+		     $in=false,
+		     $limit=0,
+		     $offset=0,
+		     $sort='registry_objects.registry_object_id',
+		     $sortd='asc')
+	{
+		if (is_array($in) and count($in) == 0)
+		{
+			return array();
+		}
+		else
+		{
+			$this->db->distinct()->select("registry_objects.registry_object_id")
+				->join("data_sources",
+				       "data_sources.data_source_id = registry_objects.data_source_id")
+				->join("registry_object_attributes",
+				       "registry_object_attributes.registry_object_id = registry_objects.registry_object_id")
+				->order_by($sort, $sortd);
+			if ($in !== false)
+			{
+				$this->db->where_in("registry_objects.registry_object_id", $in);
+			}
+			$query = $this->db->get_where('registry_objects',
+						      $where, $limit, $offset);
+
+
+			if ($query->num_rows() > 0)
+			{
+				$records = array_map(create_function('$r',
+								     $this->_mkro_callback),
+						     $query->result_array());
+			}
+			else
+			{
+				$records = array();
+			}
+			$query->free_result();
+		}
+		return $records;
+	}
+
 	/**
 	 * Returns exactly one registry object by Key (or NULL)
-	 * 
+	 *
 	 * @param the registry object key
 	 * @return _registry_object object or NULL
 	 */
@@ -37,23 +94,23 @@ class Registry_objects extends CI_Model {
 			$query->free_result();
 			return new _registry_object($id[0]['registry_object_id']);
 		}
-	} 	
-	
+	}
+
 	/**
 	 * Returns exactly one registry object by Key (or NULL)
-	 * 
+	 *
 	 * @param the registry object key
 	 * @return _registry_object object or NULL
 	 */
 	function getByID($id)
 	{
 		return new _registry_object($id);
-	} 	
-	
-	
+	}
+
+
 	/**
 	 * Returns exactly one registry object by URL slug (or NULL)
-	 * 
+	 *
 	 * @param the registry object slug
 	 * @return _registry_object object or NULL
 	 */
@@ -71,12 +128,12 @@ class Registry_objects extends CI_Model {
 			$query->free_result();
 			return new _registry_object($id[0]['registry_object_id']);
 		}
-	} 	
-	
-	
+	}
+
+
 	/**
 	 * Get a number of registry_objects that match the attribute requirement (or an empty array)
-	 * 
+	 *
 	 * @param the name of the attribute to match by
 	 * @param the value that the attribute must match
 	 * @return array(_registry_object)
@@ -103,11 +160,11 @@ class Registry_objects extends CI_Model {
 		$query->free_result();
 		//var_dump($matches);
 		return $matches;
-	} 	
-	
+	}
+
 	/**
 	 * Get a number of registry_objects that match the attribute requirement (or an empty array)
-	 * 
+	 *
 	 * @param the data source ID to match by
 	 * @return array(_registry_object)
 	 */
@@ -124,11 +181,11 @@ class Registry_objects extends CI_Model {
 		}
 		$query->free_result();
 		return $matches;
-	} 	
+	}
 
 	/**
 	 * Get a number of registry_objects that match the attribute requirement (or an empty array)
-	 * 
+	 *
 	 * @param the data source ID to match by
 	 * @return array(_registry_object)
 	 */
@@ -145,48 +202,60 @@ class Registry_objects extends CI_Model {
 		}
 		$query->free_result();
 		return $matches;
-	} 	
-	
-	
+	}
+
+
 	/**
-	 * XXX: 
+	 * Get a number of registry_objects that match the class requirement (or an empty array)
+	 *
+	 * @param the value that the class must match
+	 * @return array(_registry_object)
+	 */
+	function getByClass($class)
+	{
+		return $this->get(array("class"=>$class));
+	}
+
+
+	/**
+	 * XXX:
 	 * @return array(_data_source) or NULL
 	 */
 	function create($data_source_key, $registry_object_key, $class, $title, $status, $slug, $record_owner)
 	{
 		if (is_null($this->getByKey($registry_object_key)))
 		{
-			
+
 			$ro = new _registry_object();
-			
-			// Get the data_source_id for this data source key		
+
+			// Get the data_source_id for this data source key
 			$this->load->model('data_source/data_sources','ds');
 			$ds = $this->ds->getByKey($data_source_key);
 			$ro->_initAttribute("data_source_id", $ds->getAttribute('data_source_id'), TRUE);
-	
-	
+
+
 			$ro->_initAttribute("key",$registry_object_key, TRUE);
 			$ro->_initAttribute("class",$class, TRUE);
 			$ro->_initAttribute("title",$title, TRUE);
 			$ro->_initAttribute("status",$status, TRUE);
 			$ro->_initAttribute("slug",$slug, TRUE);
 			$ro->_initAttribute("record_owner",$record_owner, TRUE);
-			
+
 			// Some extras
 			$ro->setAttribute("created",time());
-	
+
 			$ro->create();
 			return $ro;
-			
+
 		}
 		else
 		{
 			return $this->update($registry_object_key, $class, $title, $status, $slug, $record_owner);
 		}
-	} 	
-	
+	}
+
 	/**
-	 * XXX: 
+	 * XXX:
 	 * @return array(_data_source) or NULL
 	 */
 	function update($registry_object_key, $class, $title, $status, $slug, $record_owner)
@@ -201,17 +270,17 @@ class Registry_objects extends CI_Model {
 			$ro->setAttribute("slug",$slug);
 			$ro->setAttribute("record_owner",$record_owner);
 
-			$ro->save();		
-			return $ro;	
+			$ro->save();
+			return $ro;
 		}
 		else
 		{
-			throw new Exception ("Unable to update registry object (this registry object key does not exist in the registry)");	
+			throw new Exception ("Unable to update registry object (this registry object key does not exist in the registry)");
 		}
-	} 	
-	
-	
-	
+	}
+
+
+
 	/**
 	 * @ignore
 	 */
@@ -219,6 +288,6 @@ class Registry_objects extends CI_Model {
 	{
 		parent::__construct();
 		include_once("_registry_object.php");
-	}	
-		
+	}
+
 }
