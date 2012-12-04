a0898cabd5fa7910d49bc7c2f84919cfe06a00ca
Leo Monus <u4187959@indx.services.ands.org.au>
2012-12-04 13:59:50 +1100

    migrating migraine

--- arms/src/applications/registry/install/controllers/migrate.php
+++ arms/src/applications/registry/install/controllers/migrate.php
@@ -27,25 +27,61 @@ class Migrate extends MX_Controller
 		}
 
 		// Start the clock...
+
 		$this->exec_time = microtime(true);
 		$this->load->model('data_source/data_sources','ds');
-
-
 		foreach ($query->result() AS $result)
 		{
 			echo "Importing data source: " . $result->title . ".";
-			$data_source = $this->ds->create($result->data_source_key, $result->title); // XXX: Generate slug.
+			//var_dump($result);
+			//exit();
+			$data_source = $this->ds->getByKey($result->data_source_key);
+			if($data_source === NULL)
+				$data_source = $this->ds->create($result->data_source_key, $result->title); // XXX: Generate slug.
+			
 			$data_source->title = $result->title;
-			// etc...
-
+			$data_source->setAttribute("provider_type", $result->provider_type);
+			$data_source->setAttribute("uri", $result->uri);
+			$data_source->setAttribute("contact_name", $result->contact_name);
+			$data_source->setAttribute("contact_email", $result->contact_email);
+			$data_source->setAttribute("notes", substr($result->notes, 0 ,255));
+			$data_source->setAttribute("record_owner", $result->record_owner);
+			$data_source->setAttribute("created_when", $result->created_when);
+			$data_source->setAttribute("modified_when", $result->modified_when);
+			$data_source->setAttribute("modified_who", $result->modified_who);
+			$data_source->setAttribute("harvest_method", $result->harvest_method);
+			$data_source->setAttribute("oai_set", $result->oai_set);
+			$data_source->setAttribute("harvest_date", $result->harvest_date);
+			$data_source->setAttribute("harvest_frequency", $result->harvest_frequency);
+			$data_source->setAttribute("isil_value", $result->isil_value);
+			$data_source->setAttribute("push_to_nla", $result->push_to_nla);
+			$data_source->setAttribute("allow_reverse_internal_links", $result->allow_reverse_internal_links);
+			$data_source->setAttribute("allow_reverse_external_links", $result->allow_reverse_external_links);
+			$data_source->setAttribute("assessement_notify_email_addr", $result->assessement_notification_email_addr);
+			$data_source->setAttribute("auto_publish", $result->auto_publish);
+			$data_source->setAttribute("qa_flag", $result->qa_flag);
+			$data_source->setAttribute("create_primary_relationships", $result->create_primary_relationships);
+			$data_source->setAttribute("primary_key_1", $result->primary_key_1);
+			$data_source->setAttribute("class_1", $result->class_1);
+			$data_source->setAttribute("collection_rel_1", $result->collection_rel_1);
+			$data_source->setAttribute("party_rel_1", $result->party_rel_1);
+			$data_source->setAttribute("activity_rel_1", $result->activity_rel_1);
+			$data_source->setAttribute("service_rel_1", $result->service_rel_1);
+			$data_source->setAttribute("primary_key_2", $result->primary_key_2);
+			$data_source->setAttribute("class_2", $result->class_2);
+			$data_source->setAttribute("collection_rel_2", $result->collection_rel_2);
+			$data_source->setAttribute("party_rel_2", $result->party_rel_2);
+			$data_source->setAttribute("activity_rel_2", $result->activity_rel_2);
+			$data_source->setAttribute("service_rel_2", $result->service_rel_2);
+			$data_source->setAttribute("time_zone_value", $result->time_zone_value);
 			$data_source->save();
-
-			echo ".";
+			$data_source->append_log("IMPORTED FROM: ".$this->source->database, "info");
+			echo "." .NL;
 
 
 			// Now start importing registry objects
 			$this->migrateRegistryObjectsForDatasource($data_source);
-
+			echo $data_source->key;
 			echo ". complete!" . NL;
 		}
 
@@ -56,8 +92,9 @@ class Migrate extends MX_Controller
 
 	function migrateRegistryObjectsForDatasource(_data_source $data_source)
 	{
-		// Do some stuff here...
-		echo "Called migrateRegistryObjectsForDatasource()" . NL;
+		$query = $this->source->get_where("dba.tbl_registry_objects", array("data_source_key"=>$data_source->key));
+		$num_records = $query->num_rows();
+		echo "FOUND: ". $num_records . "records" .NL;
 	}
 
 
