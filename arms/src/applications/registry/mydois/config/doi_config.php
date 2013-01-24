60f882e307bfeff5205b95c2837594817b4c197e
Elizabeth Woods <u3485947@indx.services.ands.org.au>
2013-01-25 09:20:10 +1100

    added reference to global environment variable

--- arms/src/applications/registry/mydois/config/doi_config.php
+++ arms/src/applications/registry/mydois/config/doi_config.php
@@ -1,8 +1,9 @@
 <?php
+global $ENV;
 $config['test_doi_prefix'] = '10.5072';
 $config['gDOIS_SERVICE_BASE_URI'] = "https://mds.datacite.org/";
 $config['gDOIS_DATACENTRE_NAME_PREFIX'] = "ANDS";
 $config['gDOIS_DATACENTRE_NAME_MIDDLE'] = "CENTRE";
-$config['gDOIS_DATACITE_PASSWORD'] = "and3do1p455";
+$config['gDOIS_DATACITE_PASSWORD'] = $ENV['gDOIS_DATACITE_PASSWORD'];
 $config['gDOIS_RESPONSE_SUCCESS'] = "OK";
 ?>
