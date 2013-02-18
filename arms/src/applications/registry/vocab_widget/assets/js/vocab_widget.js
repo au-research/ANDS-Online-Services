7673e9562ca2eb07611124bbb215f8f40a9024ca
Steven McPhillips <steven.mcphillips@gmail.com>
2013-02-18 12:17:30 +1100

    vocab widget: added 'narrow' mode, which resulted in a fairly hefty refactor of the widget. 'search' and 'narrow' modes are now handled by a subclass of a VocabHandler js object/interface, which provides some rudimetary helper routines.
    The codebase is still a bit messy. Need to clean up:
      - settings (split by mode?)
      - validation (push back to VocabHandler, subclasses provide rules?)
    
    And of course the plugin documentation needs updating.

--- arms/src/applications/registry/vocab_widget/assets/js/vocab_widget.js
+++ arms/src/applications/registry/vocab_widget/assets/js/vocab_widget.js
@@ -1,44 +1,291 @@
 /*
-Copyright 2009 The Australian National University
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+  Copyright 2009 The Australian National University
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0
+  http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 *******************************************************************************/
-
 ;(function($) {
     $.fn.ands_vocab_widget = function(options) {
-	var WIDGET_NAME = "ANDS Vocabulary Widget service"
-	var WIDGET_ID = "ands_vocab_widget_list";
+	var VocabHandler = Class.extend({
+	    init: function(container, settings) {
+		this._container = container;
+		this.settings = settings;
+	    },
+
+	    /**
+	     * Implemented by subclasses; prep widget
+	     * for user interaction
+	     */
+	    ready: function() {
+		return false;
+	    },
+
+	    /**
+	     * Implemented by subclasses; disable
+	     * widget's user interaction
+	     */
+	    detach: function() {
+		return false;
+	    },
+
+	    /**
+	     * basic error handler
+	     */
+	    _err: function(xhr) {
+		if (typeof(this.settings['error_msg']) === 'boolean' &&
+		    this.settings['error_msg'] === false) {
+		}
+		else {
+		    alert(this.settings['error_msg'] + "\r\n\r\n"
+			  + xhr.responseText +
+			  "\r\n(id:" + this._container.attr('id') + ")");
+		}
+		this._container.blur();
+		return false;
+	    },
+
+	});
+
+	var SearchHandler = VocabHandler.extend({
+	    init: function(container, settings) {
+		this._super(container, settings);
+
+		$('<ul id="' + container.attr('id') + settings._wid + '"' +
+		  'class="' + settings.list_class + '" />').insertAfter(container);
+		this._list = $("ul#" + container.attr('id') + settings._wid).hide();
+
+		if (!this._container.is("input[type='text']")) {
+		    // we only like being attached to input elements when searching
+		    this._err({status: 500,
+			       responseText: "must be attached to a text " +
+			       "input element when mode is 'search'"});
+		    return false;
+		}
+		//disable autocomplete; interferes with our autocomplete
+		this._container.attr("autocomplete", "off");
+	    },
+
+	    ready: function() {
+		var handler = this;
+		this._container.bind("keydown", function() { handler.vocab_lookup(); });
+	    },
+
+	    detach: function() {
+		this._container.unbind("keydown");
+	    },
+
+	    /**
+	     * let's do something with the provided data
+	     */
+	    process: function(data) {
+		var handler = this;
+
+		if (data.status !== "OK") {
+		    this._err({status: 500, responseText: data.message});
+		}
+		else {
+		    this._reset();
+		    if (data.count === 0 &&
+			typeof(this.settings['nohits_msg']) !== 'boolean' &&
+			typeof(this.settings['nohits_msg']) !== false) {
+			this._list.append('<li role="vocab_error">' +
+					  this.settings['nohits_msg'] +
+					  '</li>');
+		    }
+		    else if (data.count === 0 &&
+			     typeof(this.settings['nohits_msg']) === 'boolean' &&
+			     this.settings['nohits_msg'] === false) {
+			this._list.empty();
+		    }
+		    else if (data.count > 0) {
+			$.each(data.items, function(idx, item) {
+			    handler._list.append(handler.vocab_item(item));
+			});
+		    }
+		    else {
+			this._list.append('<li role="vocab_error">' +
+					  'Hmmm... something went wrong here.' +
+					  ' Try again?</li>');
+		    }
+		    this._list.show()
+			.children('li[role="vocab_item"]')
+			.bind('click', function(event) { handler.handle_selection(event)});
+		}
+	    },
+
+	    /**
+	     * make the ajax call using the plugin settings + input value.
+	     * calls `process` on success, `_err` on error
+	     */
+	    lookup: function() {
+		if (this._container.val().length >= this.settings.min_chars) {
+		    var handler = this;
+		    var url = this.settings.endpoint +
+			"?action=" + this.settings.mode +
+			"&repository=" + this.settings.repository +
+			"&limit=" + this.settings.max_results +
+			"&lookfor=" + this._container.val();
+		    $.ajax({
+			url: url,
+			cache: this.settings.cache,
+			dataType: "jsonp",
+			success: function(data) { handler.process(data) },
+			error: function(xhr) { handler._err(xhr) },
+		    });
+		}
+	    },
+
+	    /**
+	     * generate the display for a vocab item (i.e. list item)
+	     */
+	    vocab_item: function(data) {
+		var item = $('<li role="vocab_item" />');
+		item.data('ands_vocab_data', data);
+		$.each(this.settings.fields, function(idx, field) {
+		    if (typeof(data[field]) !== 'undefined') {
+			item.append('<span role="' + field + '">' +
+				    data[field] +
+				    '</span>');
+		    }
+		});
+		return item;
+	    },
+
+
+	    /**
+	     * reset the list
+	     */
+	    _reset: function() {
+		this._list.empty();
+		this._list.hide();
+	    },
+
+
+	    /**
+	     * once a selection has been made, we need to do something with it
+	     */
+	    handle_selection: function(event) {
+		var data = $(event.target).parent().data('ands_vocab_data');
+		if (typeof(data[this.settings.target_field]) !== 'undefined') {
+		    this._container.val(data[this.settings.target_field]);
+		    this._reset();
+		}
+		else {
+		    this._err({status: 404,
+			       responseText: 'item is missing target field (' +
+			       this.settings.target_field + ')'});
+		}
+	    },
+
+	    /**
+	     * silly wrapper to provide input buffering.
+	     * `lookup` makes the ajax call
+	     */
+	    vocab_lookup: function (event) {
+		this._reset();
+		if (this._container.data('ands_vocab_timer')) {
+		    clearTimeout(this._container.data('ands_vocab_timer'));
+		}
+		this._container.data('ands_vocab_timer',
+				     setTimeout(this.lookup(),
+						this.settings.delay));
+	    },
+
+
+	});
+
+	var NarrowHandler = VocabHandler.extend({
+	    init: function(container, settings) {
+		this._super(container, settings);
+
+		if (!this._container.is("select")) {
+		    this._err({status:500,
+			       responseText: "must be attached to a select " +
+			       "element when mode isn't 'search'"});
+		}
+
+		if (this.settings.fields.length > 1) {
+		    this._err({status:500,
+			       responseText:"'fields' setting must be a single" +
+			       " element array when mode isn't 'search'"});
+		}
+
+		this._container.empty().append('<option value=""></option>');
+	    },
+
+	    ready: function() {
+		var handler = this;
+		var url = this.settings.endpoint +
+		    "?action=" + this.settings.mode +
+		    "&repository=" + this.settings.repository +
+		    "&limit=" + this.settings.max_results +
+		    "&lookfor=" + this.settings.mode_params;
+		$.ajax({
+		    url: url,
+		    cache: this.settings.cache,
+		    dataType: "jsonp",
+		    success: function(data) { handler.process(data) },
+		    error: function(xhr) { handler._err(xhr) },
+		});
+	    },
+
+	    process: function(data) {
+		var handler = this;
+		if (data.status === "OK") {
+		    $.each(data.items, function(idx, item) {
+			var val = item[handler.settings.target_field];
+			var label = item[handler.settings.fields[0]];
+			handler._container.append('<option value="' + val + '">' +
+						  label + '</option>');
+		    });
+		}
+		else {
+		    handler._err({status:500,
+			       responseText:data.message});
+		}
+	    },
+
+	});
+
+	var WIDGET_NAME = "ANDS Vocabulary Widget service";
+	var WIDGET_ID = "_vocab_widget_list";
 	var defaults = {
 	    //location (absolute URL) of the jsonp proxy
 	    endpoint: 'http://ands3.anu.edu.au/workareas/smcphill/ands-online-services/arms/src/registry/vocab_widget/proxy/',
 
-	    //currently, only search is available. in future, 'narrow',
+	    //sisvoc repository to query. (proxy defaults to 'anzsrc-for' if none supplied)
+	    repository: 'anzsrc-for',
+
+	    //currently, 'search' and 'narrow' are available. in future,
 	    //'broad', 'concepts' modes will be added
 	    mode: "search",
+
+	    //search doesn't require any parameters, but narrow does (and broaden will)
+	    //in the latter case, the parameter is the URI to narrow/broaden on
+	    mode_params: "",
 
 	    //at most, how many results should be returned?
 	    max_results: 100,
 
-	    // how many characters are required before we send a query?
+	    //search mode: how many characters are required before we send a query?
 	    min_chars: 3,
 
-	    //how long should we wait (after initial user input) before
+	    //search mode: how long should we wait (after initial user input) before
 	    //firing the search? provide in milliseconds
 	    delay: 500,
 
 	    //should we cache results? yes by default
 	    cache: true,
 
-	    //what to show when no hits? set to boolean(false) to supress
+	    //search mode: what to show when no hits? set to boolean(false) to supress
 	    nohits_msg: "No matches found",
 
 	    //what to show when there's some weird error? set to boolean(false)
@@ -48,20 +295,28 @@ limitations under the License.
 	    //provide CSS 'class' references. Separate multiple classes by spaces
 	    list_class: "ands_vocab_list",
 
-	    //which fields do you want to display? currently,
-	    //('label', 'notation', 'about') are available
+	    //which fields do you want to display? check the repository for available fields
+	    //nb:
+	    //  - anzsrc-for uses [label, notation, about]
+	    //  - rifcs uses [label, definition, about]
+	    //
+	    //nb: in browse mode, this should be a single element array
 	    fields: ['label', 'notation', 'about'],
 
 	    //what data field should be stored upon selection?
+	    //in narrow mode, this is the option's value attribute
 	    target_field: "label",
 
 	},
 	settings = $.extend({}, defaults, options);
 
 	//do some quick and nasty fixes
-	settings.list_class = typeof(settings.list_class) == 'undefined' ?
+	settings.list_class = typeof(settings.list_class) === 'undefined' ?
 	    "" :
 	    settings.list_class;
+
+	settings._wname = WIDGET_NAME;
+	settings._wid = WIDGET_ID;
 
 	/**
 	 * simplistic throwable template for input validation
@@ -75,22 +330,22 @@ limitations under the License.
 	 */
 	function valid_input() {
 	    var is_valid = true;
-
+	    var container = $(this);
 	    var validation_rules = [
 		{
 		    fields: ["min_chars", "max_results", "delay"],
 		    description: "a positive integer",
-		    test: function(val) { return typeof(val) == 'number' && String(val) == ~~Number(val) && val >= 0; }
+		    test: function(val) { return typeof(val) === 'number' && val === ~~Number(val) && val >= 0; }
 		},
 		{
 		    fields: ["cache"],
 		    description: "a boolean",
-		    test: function(val) { return typeof(val) == 'boolean'; }
+		    test: function(val) { return typeof(val) === 'boolean'; }
 		},
 		{
 		    fields: ["mode"],
-		    description: "one of <search,>",
-		    test: function(val) { return val == 'search'; }
+		    description: "one of <search,narrow>",
+		    test: function(val) { return val === 'search' || val === 'narrow'; }
 		},
 		{
 		    fields: ["endpoint"],
@@ -98,20 +353,30 @@ limitations under the License.
 		    test: function(val) { var urlre = new RegExp("^(http|https)\://.*$"); return urlre.test(val); }
 		},
 		{
-		    fields: ["list_class"],
+		    fields: ["list_class", "repository"],
 		    description: "a string",
-		    test: function(val) { return typeof(val) == 'undefined' || typeof(val) == 'string'; }
+		    test: function(val) { return typeof(val) === 'undefined' || typeof(val) === 'string'; }
 		},
 		{
 		    fields: ["fields"],
 		    description: "an array of strings",
-		    test: function(val) { return Object.prototype.toString.call(val) == "[object Array]"; }
+		    test: function(val) { return Object.prototype.toString.call(val) === "[object Array]"; }
+		},
+		{
+		    fields: ["mode_params"],
+		    description: "non-search mode parameters",
+		    test: function(val) { return ((typeof(val) !== 'undefined' && settings['mode'] !== 'search') || settings['mode'] === 'search'); }
 		},
 	    ];
 
 	    $.each(validation_rules, function(ridx, rule) {
 		$.each(rule.fields, function(fidx, field) {
-		    is_valid = is_valid && rule.test(settings[field]);
+		    try {
+			is_valid = is_valid && rule.test(settings[field]);
+		    }
+		    catch (e) {
+			is_valid = false;
+		    }
 		    if (!is_valid) {
 			_throwing(field, rule.description);
 			return false;
@@ -129,151 +394,21 @@ limitations under the License.
 	try {
 	    is_ok = valid_input();
 	}
-	catch (e) {}
+	catch (e) { }
 
+	var handler;
 	if (is_ok) {
 	    return this.each(function() {
 		var $this = $(this);
-		$('<ul id="' + WIDGET_ID + '"' +
-		  'class="' + settings.list_class +
-		  '" />').insertAfter($this);
-		var $list = $("ul#" + WIDGET_ID);
-		$list.hide();
-
-		if (settings.mode == 'search') {
-		    if (!$this.is("input[type='text']")) {
-			// we only like being attached to input elements
-			alert(WIDGET_NAME + ': must be attached to a text input element!');
-			return false;
-		    }
-		    //disable autocomplete; interferes with our autocomplete
-		    $this.attr("autocomplete", "off");
+		switch(settings.mode) {
+		case 'search':
+		    handler = new SearchHandler($this, settings);
+		    break;
+		case 'narrow':
+		    handler = new NarrowHandler($this, settings);
 		}
 
-		/**
-		 * silly wrapper to provide input buffering.
-		 * _lookup makes the ajax call
-		 */
-		function vocab_lookup(event) {
-		    _reset();
-		    if ($this.data('ands_vocab_timer')) {
-			window.clearTimeout($this.data('ands_vocab_timer'));
-		    }
-		    $this.data('ands_vocab_timer',
-			       window.setTimeout(_lookup, settings.delay));
-		}
-
-		/**
-		 * make the ajax call using the plugin settings + input value.
-		 * calls `process_lookup` on success, `_err` on error
-		 */
-		function _lookup() {
-		    if ($this.val().length >= settings.min_chars) {
-			var url = settings.endpoint + "?action=" + settings.mode +
-			    "&limit=" + settings.max_results + "&lookfor=" +
-			    $this.val();
-			$.ajax({
-			    url: url,
-			    cache: settings.cache,
-			    dataType: "jsonp",
-			    success: process_lookup,
-			    error: _err
-			});
-		    }
-		}
-
-		/**
-		 * basic error handler
-		 */
-		function _err(xhr) {
-		    if (typeof(settings['error_msg']) == 'boolean' &&
-			settings['error_msg'] == false) {
-		    }
-		    else {
-			alert(settings['error_msg'] + "\r\n\r\n"
-			       + xhr.status + ": " + xhr.statusText);
-		    }
-		    $this.blur();
-		}
-
-		/**
-		 * reset the list
-		 */
-		function _reset() {
-		    $list.empty();
-		    $list.hide();
-		}
-
-		/**
-		 * generate the display for a voab item (i.e. list item)
-		 */
-		function vocab_item(data) {
-		    var item = $('<li role="vocab_item" />');
-		    item.data('ands_vocab_data', data);
-		    $.each(settings.fields, function(idx, field) {
-			if (typeof(data[field]) != 'undefined') {
-			    item.append('<span role="' + field + '">' +
-					data[field] +
-					'</span>');
-			}
-		    });
-		    return item;
-		}
-
-		/**
-		 * once a selection has been made, we need to do something with it
-		 */
-		function handle_selection(event) {
-		    var data = $(this).data('ands_vocab_data');
-		    if (typeof(data[settings.target_field]) != 'undefined') {
-			$this.val(data[settings.target_field]);
-			_reset();
-		    }
-		    else {
-			_err({status: 404,
-			      statusText: 'item is missing target field (' +
-			      settings.target_field + ')'});
-		    }
-
-		}
-
-		/**
-		 * finally! let's do something with the provided data
-		 */
-		function process_lookup(data) {
-		    if (data.status !== "OK") {
-			_err({status: 500, statusText: data.message});
-		    }
-		    else {
-			_reset();
-			if (data.count == 0 &&
-			    typeof(settings['nohits_msg']) !== 'boolean' &&
-			    typeof(settings['nohits_msg']) !== false) {
-			    $list.append('<li role="vocab_error">' +
-					 settings['nohits_msg'] + '</li>');
-			}
-			else if (data.count == 0 &&
-				 typeof(settings['nohits_msg']) == 'boolean' &&
-				 settings['nohits_msg'] == false) {
-			    $list.empty();
-			}
-			else if (data.count > 0) {
-			    $.each(data.items, function(idx, item) {
-				$list.append(vocab_item(item));
-			    });
-			}
-			else {
-			    $list.append('<li role="vocab_error">' +
-					'Hmmm... something went wrong here. ' +
-					'Try again?</li>');
-			}
-			$list.show();
-			$('ul#' + WIDGET_ID + ' > li[role="vocab_item"]')
-			    .bind('click', handle_selection);
-		    }
-		}
-
-		$this.bind("keydown", vocab_lookup);
+		handler.ready();
 	    });
 	}
 	else {
@@ -285,9 +420,77 @@ limitations under the License.
 		alert(WIDGET_NAME + ': \r\n' + err + '\r\n(reload the page before retrying)');
 	    }
 	    //lose focus and unbind to avoid continuous errors
+	    try {
+		handler.detach();
+	    }
+	    catch (e) {}
 	    $(this).blur();
-	    $(this).unbind("keydown");
 	    return false;
 	}
     };
+
+    /* Simple JavaScript Inheritance
+     * By John Resig http://ejohn.org/
+     * MIT Licensed.
+     */
+    // Inspired by base2 and Prototype
+    (function(){
+	var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
+
+	// The base Class implementation (does nothing)
+	this.Class = function(){};
+
+	// Create a new Class that inherits from this class
+	Class.extend = function(prop) {
+	    var _super = this.prototype;
+
+	    // Instantiate a base class (but only create the instance,
+	    // don't run the init constructor)
+	    initializing = true;
+	    var prototype = new this();
+	    initializing = false;
+
+	    // Copy the properties over onto the new prototype
+	    for (var name in prop) {
+		// Check if we're overwriting an existing function
+		prototype[name] = typeof prop[name] == "function" &&
+		    typeof _super[name] == "function" && fnTest.test(prop[name]) ?
+		    (function(name, fn){
+			return function() {
+			    var tmp = this._super;
+
+			    // Add a new ._super() method that is the same method
+			    // but on the super-class
+			    this._super = _super[name];
+
+			    // The method only need to be bound temporarily, so we
+			    // remove it when we're done executing
+			    var ret = fn.apply(this, arguments);
+			    this._super = tmp;
+
+			    return ret;
+			};
+		    })(name, prop[name]) :
+		prop[name];
+	    }
+
+	    // The dummy class constructor
+	    function Class() {
+		// All construction is actually done in the init method
+		if ( !initializing && this.init )
+		    this.init.apply(this, arguments);
+	    }
+
+	    // Populate our constructed prototype object
+	    Class.prototype = prototype;
+
+	    // Enforce the constructor to be what we expect
+	    Class.prototype.constructor = Class;
+
+	    // And make this class extendable
+	    Class.extend = arguments.callee;
+
+	    return Class;
+	};
+    })();
 })( jQuery );
