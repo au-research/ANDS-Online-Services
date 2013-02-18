7da9319430d6b423837e6fe0110aec50d1404e2d
Steven McPhillips <steven.mcphillips@gmail.com>
2013-02-18 12:20:36 +1100

    vocab widget: updated proxy for rifcs testing

--- arms/src/applications/registry/vocab_widget/views/proxy.php
+++ arms/src/applications/registry/vocab_widget/views/proxy.php
@@ -23,7 +23,8 @@ limitations under the License.
  */
 
 // some constants
-define("BASE_URL", "http://devl.ands.org.au:8080/sissvoc/api/");
+define("BASE_URL", "http://devl.ands.org.au:8080/sissvoc/api/"); //use this for testing rifcs
+//define("BASE_URL", "http://ands3.anu.edu.au:8080/sissvoc/api/"); //use this for testing anzsrc-for
 define("SEARCH_URL", "/concept.json?anycontains=");
 define("NARROW_URL", "/concept/allNarrower.json?uri=");
 define("BROAD_URL", "/concept/allBroader.json?uri="); #future use
