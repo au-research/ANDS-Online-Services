77fb36db612fcd11083225a4866c039cbf5e9e64
Minh Nguyen <dekarvn@gmail.com>
2013-03-13 09:31:51 +1100

    fix footer

--- arms/src/applications/portal/home/assets/js/home_page.js
+++ arms/src/applications/portal/home/assets/js/home_page.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
 	//Featured Research Domain
 	$.getJSON(default_base_url+'registry/services/rda/getSpotlightPartners/',initSpotlight);
 	function initSpotlight(data){
-		console.log(data);
 		var template = $('#spotlight_template').html();
 		var output = Mustache.render(template, data);
 		$('#spotlight').html(output);
