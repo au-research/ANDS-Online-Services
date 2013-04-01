e5426b168e8e8c4fb0f3584cf04996f2ef25f16e
Ben Greenwood <ben.greenwood@anu.edu.au>
2013-04-02 09:12:35 +1100

    Fix gold standard records display when none exist

--- arms/src/applications/registry/registry_object/views/registry_object_list.php
+++ arms/src/applications/registry/registry_object/views/registry_object_list.php
@@ -20,23 +20,27 @@
 				<div class="widget-title stick"><h5><?php echo $list_title;?></h5></div>
 				<div class="widget-content nopadding">
 					<ul class="sortable">
-						<?php foreach($ros as $ro):?>
-						<li>
-							<div class="ro_title"><?php echo anchor('registry_object/view/'.$ro->id, $ro->title);?></div>
-							<div class="ro_content ">
-								<p>
-									<span class="tag" tip="Last Modified"><i class="icon icon-time"></i><?php echo timeAgo($ro->last_updated);?></span>
-									<img class="tag" tip="<?php echo $ro->class;?>" src="<?php echo asset_url('img/'.$ro->class.'.png', 'base');?>"/>
-									<span class="tag gold_status_flag" tip="<h5>Gold Standard</h5><p>The following record has been verified<br/> as an exemplary record <br/>by the ANDS Metadata Assessment Group.</p>"><i class="icon icon-star-empty"></i></span>
-								</p>
-							</div>
-							<div class="btn-group btn-group-vertical right-menu hide">
-								<button class="contextmenu btn btn-small" status="{{name}}"><i class="icon icon-wrench"></i></button>
-								<button class="tipTag btn btn-small" ro_id="{{id}}"><i class="icon icon-tag"></i></button>
-							</div>
-							<div class='clearfix'></div>
-						</li>
-						<?php endforeach;?>
+						<?php if (!is_array($ros)): ?> 
+							<li><div class="ro_content"><strong> No records to display&hellip;</strong></div></li>
+						<?php else: ?>
+							<?php foreach($ros as $ro):?>
+							<li>
+								<div class="ro_title"><?php echo anchor('registry_object/view/'.$ro->id, $ro->title);?></div>
+								<div class="ro_content">
+									<p>
+										<span class="tag" tip="Last Modified"><i class="icon icon-time"></i><?php echo timeAgo($ro->last_updated);?></span>
+										<img class="tag" tip="<?php echo $ro->class;?>" src="<?php echo asset_url('img/'.$ro->class.'.png', 'base');?>"/>
+										<span class="tag gold_status_flag" tip="<h5>Gold Standard</h5><p>The following record has been verified<br/> as an exemplary record <br/>by the ANDS Metadata Assessment Group.</p>"><i class="icon icon-star-empty"></i></span>
+									</p>
+								</div>
+								<div class="btn-group btn-group-vertical right-menu hide">
+									<button class="contextmenu btn btn-small" status="{{name}}"><i class="icon icon-wrench"></i></button>
+									<button class="tipTag btn btn-small" ro_id="{{id}}"><i class="icon icon-tag"></i></button>
+								</div>
+								<div class='clearfix'></div>
+							</li>
+							<?php endforeach;?>
+						<?php endif; ?>
 					</ul>
 				</div>
 			</div>
