9caf9568e38e476448d913c14cc4b6da55a7afe5
Leo Monus <u4187959@indx.services.ands.org.au>
2013-04-11 14:08:46 +1000

    generate map preview if deleted record pops up (final)

--- arms/src/applications/portal/search/assets/js/search.js
+++ arms/src/applications/portal/search/assets/js/search.js
@@ -738,7 +738,7 @@ function showPreviewWindowConent(mOverlay)
     {
     	//log(mOverlay);
     	$(mOverlay.markers_).each(function(){
-    	roIds.push(this.id + 355555);
+    	roIds.push(this.id);
     	});
     	infowindow.setPosition(mOverlay.center_)
     }
