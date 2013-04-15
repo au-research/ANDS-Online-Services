26d0fa84a59dc00273c620ee0a3b633ddfe1897b
Ben Greenwood <ben.greenwood@anu.edu.au>
2013-04-15 15:25:53 +1000

    Fix typo semicolon

--- arms/db/mysql/dbs_portal_r9_to_r10_incremental.sql
+++ arms/db/mysql/dbs_portal_r9_to_r10_incremental.sql
@@ -4,6 +4,6 @@ CREATE TABLE `search_result_counts` (
   `timestamp` int(11) DEFAULT NULL,
   `search_term` varchar(256) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 ALTER TABLE `page_views` ADD COLUMN `registry_object_id` int(11) DEFAULT NULL;
