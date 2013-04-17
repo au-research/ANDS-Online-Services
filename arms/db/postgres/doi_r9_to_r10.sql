b9b8fd4f88b136348c9f854563c75778b34e7a15
Leo Monus <u4187959@indx.services.ands.org.au>
2013-04-17 10:54:46 +1000

    DOI clensing ++

--- arms/db/postgres/doi_r9_to_r10.sql
+++ arms/db/postgres/doi_r9_to_r10.sql
@@ -1,3 +1,4 @@
+-- JUST TO BE SURE
 ALTER TABLE doi_alternate_identifiers DROP CONSTRAINT "doi_id";
 ALTER TABLE doi_contributors DROP CONSTRAINT "doi_id";
 ALTER TABLE doi_creators DROP CONSTRAINT "doi_id";
@@ -8,3 +9,16 @@ ALTER TABLE doi_related_identifiers DROP CONSTRAINT "doi_id";
 ALTER TABLE doi_resource_types DROP CONSTRAINT "doi_id";
 ALTER TABLE doi_sizes DROP CONSTRAINT "doi_id";
 ALTER TABLE doi_subjects DROP CONSTRAINT "doi_id";
+
+-- DELETE TEST DOI METADATA (We shouldn't use them by now !!)
+
+delete from doi_alternate_identifiers where doi_id like '10.5072/%';
+delete from doi_contributors where doi_id like '10.5072/%';
+delete from doi_creators where doi_id like '10.5072/%';
+delete from doi_dates where doi_id like '10.5072/%';
+delete from doi_descriptions where doi_id like '10.5072/%';
+delete from doi_formats where doi_id like '10.5072/%';
+delete from doi_related_identifiers where doi_id like '10.5072/%';
+delete from doi_resource_types where doi_id like '10.5072/%';
+delete from doi_sizes where doi_id like '10.5072/%';
+delete from doi_subjects where doi_id like '10.5072/%';
