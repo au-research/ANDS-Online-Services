b2c343480104e7e502ac129dd43156087477e379
Elizabeth Woods <u3485947@indx.services.ands.org.au>
2013-04-19 13:20:47 +1000

    removed the reverse relationship and expnading text of relationships from registry view

--- arms/src/applications/registry/registry_object/models/extensions/connections.php
+++ arms/src/applications/registry/registry_object/models/extensions/connections.php
@@ -6,88 +6,7 @@ class Connections_Extension extends ExtensionBase
 
 	private $party_one_types = array('person','administrativePosition');
 	private $party_multi_types = array('group');
-	public $typeArray = array("collection" => array(
-    "describes" => array("Describes", "Described by"),
-    "hasAssociationWith" => array("Associated with", "Associated with"),
-    "hasCollector" => array("Aggregated by", "Collector of"),
-    "hasPart" => array("Contains", "Part of"),
-    "isDescribedBy" => array("Described by","Describes"),
-    "isLocatedIn" => array("Located in", "Location for"),
-    "isLocationFor" => array("Location for","Located in"),
-    "isManagedBy" => array("Managed by","Manages"),
-    "isOutputOf" => array("Output of","Outputs"),
-    "isOwnedBy" => array("Owned by","Owns"),
-    "isPartOf" => array("Part of","Contains"),
-    "supports" => array("Supports", "Supported by"),
-    "enriches" =>array("Enriches", "Enriched by"),
-    "isEnrichedBy" =>array("Enriched by", "Enriches"),
-    "makesAvailable" =>array("Makes available", "Available through"),
-    "isPresentedBy" =>array("Presented by", "Presents"),
-    "presents" =>array("Presents", "Presented by"),
-    "isDerivedFrom" =>array("Derived from", "Derives"),
-    "hasDerivedCollection" =>array("Derives", "Derived From"),
-    "supports" =>array("Supports", "Supported by"), 
-    "isAvaiableThrough" =>array("Available through", "Makes available"),    
-    "isProducedBy" =>array("Produced by", "Produces"),
-    "produces" =>array("Produces", "Produced by"),
-    "isOperatedOn" =>array("Operated on", "Operates on"),
-    "hasPrincipalInvestigator" =>array("Principal investigator", "Principal investigator of"),
-    "isPrincipalInvestigator" =>array("Principal investigator of", "Principal investigator"),
-    "isFunderOf" => array("Funds","Funded by"),   
-),
-"party" => array(
-    "hasAssociationWith" => array("Associated with", "Associated with"),
-    "hasMember" => array("Has member", "Member of"),
-    "hasPart" => array("Has part", "Part of"),
-    "isCollectorOf" => array("Collector of","Collected by"),
-    "isFundedBy" => array("Funded by","Funds"),
-    "isFunderOf" => array("Funds","Funded by"),
-    "isManagedBy" => array("Managed by","Manages"),
-    "isManagerOf" => array("Manages","Managed by"),
-    "isMemberOf" => array("Member of","Has memeber"),
-    "isOwnedBy" => array("Owned by","Owns"),
-    "isOwnerOf" => array("Owner of", "Owned by"),
-    "isParticipantIn" => array("Participant in","Part of"),
-    "isPartOf" => array("Part of","Participant in"),
-    "enriches" =>array("Enriches", "Enriched by"),
-    "makesAvailable" =>array("Makes available", "Available through"),
-    "isEnrichedBy" =>array("Enriched by", "Enriches"),
-    "hasPrincipalInvestigator" =>array("Principal investigator", "Principal investigator of"),
-    "isPrincipalInvestigator" =>array("Principal investigator of", "Principal investigator"),
-),
-"service" => array(
-    "hasAssociationWith" =>  array("Associated with", "Associated with"),
-    "hasPart" => array("Includes", "Part of"),
-    "isManagedBy" => array("Managed by","Manages"),
-    "isOwnedBy" => array("Owned by","Owns"),
-    "isPartOf" => array("Part of","Has part"),
-    "isSupportedBy" => array("Supported by","Supports"),
-    "enriches" =>array("Enriches", "Enriched by"),
-    "makesAvailable" =>array("Makes available", "Available through"),
-    "isPresentedBy" =>array("Presented by", "Presents"),
-    "presents" =>array("Presents", "Presented by"),
-    "produces" =>array("Produces", "Produced by"),
-    "isProducedBy" =>array("Produced by", "Produces"),
-    "operatesOn" =>array("Operates on", "Operated by"),
-    "addsValueTo" =>array("Adds value to", "Value added by"),
-    "hasPrincipalInvestigator" =>array("Principal investigator", "Principal investigator of"),
-    "isPrincipalInvestigator" =>array("Principal investigator of", "Principal investigator"),
-    "isFunderOf" => array("Funds","Funded by"),
-),"activity" => array(
-    "hasAssociationWith" =>   array("Associated with", "Associated with"),
-    "hasOutput" => array("Produces","Output of"),
-    "hasPart" => array("Includes","Part of"),
-    "hasParticipant" => array("Undertaken by","Has participant"),
-    "isFundedBy" => array("Funded by","Funds"),
-    "isManagedBy" => array("Managed by","Manages"),
-    "isOwnedBy" => array("Owned by","Owns"),
-    "isPartOf" => array("Part of","Includes"),
-    "enriches" =>array("Enriches", "Enriched by"),
-    "makesAvailable" =>array("Makes available", "Available through"),
-    "hasPrincipalInvestigator" =>array("Principal investigator", "Principal investigator of"),
-    "isPrincipalInvestigator" =>array("Principal investigator of", "Principal investigator"),
-    "isFunderOf" => array("Funds","Funded by"),
-));  
+	
 	function __construct($ro_pointer)
 	{
 		parent::__construct($ro_pointer);
@@ -294,12 +213,6 @@ class Connections_Extension extends ExtensionBase
 			{
 				$row['origin'] = "EXPLICIT";
 			}
-			$typeArray = $this->typeArray;
-		//	print_r($typeArray[]);
-			if(isset($this->typeArray[$row['class']][$row['relation_type']]))
-			{
-			$row['relation_type'] = $this->typeArray[$row['class']][$row['relation_type']][0];
-			}
 
 			$my_connections[] = $row;
 		}
@@ -325,10 +238,6 @@ class Connections_Extension extends ExtensionBase
 		foreach ($query->result_array() AS $row)
 		{
 			$row['origin'] = "REVERSE_INT";
-			if(isset($this->typeArray[$row['class']][$row['relation_type']]))
-			{
-			$row['relation_type'] = $this->typeArray[$row['class']][$row['relation_type']][1];
-			}
 
 			$my_connections[] = $row;
 		}
@@ -353,10 +262,6 @@ class Connections_Extension extends ExtensionBase
 		foreach ($query->result_array() AS $row)
 		{
 			$row['origin'] = "REVERSE_EXT";
-			if(isset($this->typeArray[$row['class']][$row['relation_type']]))
-			{
-			$row['relation_type'] = $this->typeArray[$row['class']][$row['relation_type']][1];
-			}
 
 			$my_connections[] = $row;
 		}
