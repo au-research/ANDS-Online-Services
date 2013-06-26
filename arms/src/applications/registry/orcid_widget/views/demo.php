537bf390a17a2a228d7938b8fe6f6553cb9ec90f
Elizabeth Woods <u3485947@indx.services.ands.org.au>
2013-06-26 11:59:37 +1000

    changed look of orcid serach modal to reflect orcid org colors and logo. Changed widget so that a lookup of one orcid is possible or a search of all orcids. Formatted orcid bio info to display as a result of a lookup or as a tooltip to the search results list. Added the orcid logog file

--- arms/src/applications/registry/orcid_widget/views/demo.php
+++ arms/src/applications/registry/orcid_widget/views/demo.php
@@ -11,22 +11,19 @@
         <h2>Example Orcid Retrieval</h2>
         <hr/>
 
-        <div class="formfields">
+<input type="text" name="name" id="orcid_name1" value="" size="40" class="orcid_widget"/>
+       <!--  <div class="formfields">
           <form id="searchform">
 	    		<fieldset>
 	      			<legend>Details</legend>
               		<dl>
-						<dt>Orcid:</dt>
-						<dd><input type="text" name="name" id="orcid_name1" value="" size="40" class="orcid_lookup orcid_widget"/></dd>
+						<dd><input type="text" name="name" id="orcid_name1" value="" size="40" class="orcid_widget"/></dd>
 					</dl>
-<dl>
-						<dt>Orcid:</dt>
-						<dd><input type="text" name="name" id="orcid_name2" value="" size="40" class="orcid_lookup orcid_widget"/></dd>
-					</dl>	
+
 	    		</fieldset>
             	<p><input type="submit"/></p>
           </form>
         </div>
-      </div>
+      </div> -->
 </div>
 <?php $this->load->view('footer');?>
