b8c896b22f7b92c193d91aeb991c7e81eea05452
Minh Nguyen <dekarvn@gmail.com>
2013-06-28 12:32:37 +1000

    Publish My Data first cut

---
+++ arms/src/applications/registry/publish_my_data/controllers/publish_my_data.php
@@ -0,0 +1,56 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+//mod_enforce('mydois');
+
+/**
+ *  
+ */
+class Publish_my_data extends MX_Controller {
+
+	function index(){
+		if(!$this->user->loggedIn()){
+			$this->user->redirectLogin();
+		}else{
+			if(sizeof($this->user->affiliations())==0){
+				$data['title'] = 'Publish My Data';
+				$data['scripts'] = array('pmd');
+				$data['js_lib'] = array('core');
+				// var_dump($this->user->localIdentifier());
+				$this->load->view('pmd_create', $data);
+			}else{
+				//has affiliations, redirect
+				redirect('registry_object/add');
+			}
+		}
+	}
+
+	function publish(){
+		if($this->input->post()){
+			$org_role = 'PMD_'.$this->user->localIdentifier();
+			// echo 'creating org role: PMD_'. $this->input->post('name').'<br/>';
+			$this->load->model('cosi_authentication', 'cosi');
+			$this->cosi->createOrganisationalRole($org_role, $this->user->localIdentifier());
+			if(!$this->user->hasFunction('REGISTRY_USER') && sizeof($this->user->affiliations()==0)){
+				$this->cosi->registerAffiliation($this->user->localIdentifier(), 'REGISTRY_USER');
+				$this->cosi->registerAffiliation($this->user->localIdentifier(), $org_role);
+			}else{
+				throw Exception('an error has occured, please contact services@ands.org.au for support');
+			}
+			$this->user->refreshAffiliations($this->user->localIdentifier());
+			$this->user->appendFunction(array('REGISTRY_USER'));
+			// echo 'creating data source with title: '. $this->input->post('ds_title').' assign QA on and owner to the previous org role with the given notes<br/>';
+			$this->load->model('data_source/data_sources', 'ds');
+			$ds = $this->ds->create(url_title($this->input->post('ds_title')), url_title($this->input->post('ds_title')));
+			$ds->setAttribute('title', $this->input->post('ds_title'));
+			$ds->setAttribute('record_owner', $org_role);
+			$ds->setAttribute('qa_flag', DB_TRUE);
+			if($this->input->post('notes')) $ds->setAttribute('notes', $this->input->post('notes'));		
+			$ds->save();
+			// echo 'create a new blank rifcs with data source belongs to this one';
+			// echo '. redirect to edit that rifcs
+			redirect('registry_object/add');
+		}else{
+			throw Exception('an error has occured, please contact services@ands.org.au for support.');
+		}
+	}
+}
+	
