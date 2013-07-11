6a496d722ce65d0ac5998a577e81a86a06b0a9fd
Elizabeth Woods <u3485947@indx.services.ands.org.au>
2013-07-11 12:09:19 +1000

    Added the pull back from NLA test functionality so that demo can pull back from the nla test environment

--- arms/src/applications/registry/core/config/nla_pullback.php
+++ arms/src/applications/registry/core/config/nla_pullback.php
@@ -3,4 +3,7 @@ $config['nlaPartyPrefix'] = "http://nla.gov.au/nla.party-";  // all NLA party re
 $config['nlaPartyDataSourceKey'] = "NLA_PARTY";  // what data source do we put them into?  
 $config['nlaPartyDataSourceDefaultTitle'] = "NLA Pullback Parties DS";  // what data source do we put them into?  
 $config['nlaServiceURI'] = 'http://www.nla.gov.au/apps/srw/search/peopleaustralia';
+$config['nlaPartyDataSourceKeyTest'] = "NLA_PARTY_TEST";  // what data source do we put them into?  
+$config['nlaPartyDataSourceDefaultTitleTest'] = "Test NLA Pullback Parties DS";  // what data source do we put them into?  
+$config['nlaServiceURITest'] = 'http://www-test.nla.gov.au/apps/srw/search/peopleaustralia';
 
